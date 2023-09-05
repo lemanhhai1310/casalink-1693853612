75eba1d0c72b7210f7b80ac514315cb2e6b56897
hailm <hai.lemanh.1310@gmail.com>
2023-09-05 11:12:59 +0700

    feat: page chi_tiet_bai_viet.php

--- chi_tiet_bai_viet.php
+++ chi_tiet_bai_viet.php
@@ -2,5 +2,118 @@
 <?php $bodyClass = '' ?>
 <?php $isLogin = false ?>
 <?php require "template-parts/layouts/header.php"; ?>
+<div class="uk-section">
+    <article class="uk-article baiviet__article">
+        <div class="uk-container uk-container-small">
+            <h1 class="uk-article-title uk-text-center">Những lưu ý khi học bảng phiên âm quốc tế IPA có thể bạn chưa biết?</h1>
+            <div class="uk-text-center baiviet__box">
+                <span>Casalink</span>
+                <span>26 th2 2023</span>
+            </div>
+        </div>
+        <div class="uk-container uk-margin-medium-top uk-padding-remove">
+            <div class="uk-cover-container baiviet__thumbImg">
+                <img src="images/Rectangle-140471.png" alt="" uk-cover="">
+                <canvas width="1280" height="480"></canvas>
+            </div>
+        </div>
+        <div class="baiviet__content uk-container uk-container-small uk-margin-top uk-position-relative">
+            <div class="uk-grid-20" uk-grid>
+                <div class="uk-width-expand">
+                    <p class="uk-article-meta">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <p class="uk-article-meta">It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <p class="uk-article-meta">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                    <figure>
+                        <img src="images/Rectangle-140472.png" alt="">
+                    </figure>
+                    <p class="uk-article-meta">It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <p class="uk-article-meta">Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                    <p class="uk-article-meta">Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                    <p class="uk-article-meta">It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <hr class="uk-margin-medium-top">
+                </div>
+                <div class="uk-width-auto@m" uk-toggle="cls: baiviet__social; mode: media; media: @m">
+                    <div uk-sticky="offset: 148; end: !.baiviet__content">
+                        <div class="uk-grid uk-child-width-auto uk-flex-center uk-child-width-1-1@m uk-grid-small uk-grid-20-m" uk-grid>
+                            <div>
+                                <a href="" class="baiviet__social__icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                                        <path d="M8.08738 11.0331C8.02458 11.0331 6.70718 11.0335 6.09958 11.0329C5.78678 11.0327 5.67518 10.9203 5.67518 10.6055C5.67478 9.79688 5.67458 8.98828 5.67518 8.17968C5.67538 7.86888 5.79338 7.75028 6.10218 7.75008C6.70978 7.74968 8.01998 7.74988 8.08738 7.74988C8.08738 7.69448 8.08718 6.52708 8.08738 5.98948C8.08778 5.19468 8.22958 4.43388 8.63338 3.73728C9.04678 3.02428 9.64838 2.53588 10.4194 2.25348C10.9132 2.07248 11.4258 2.00028 11.9498 1.99988C12.6054 1.99948 13.261 2.00008 13.9168 2.00128C14.1986 2.00168 14.3236 2.12628 14.3242 2.41008C14.3254 3.17068 14.3254 3.93128 14.3242 4.69168C14.3238 4.97848 14.204 5.09368 13.9158 5.09688C13.3784 5.10268 12.8406 5.09908 12.3038 5.12068C11.7616 5.12068 11.4764 5.38548 11.4764 5.94668C11.4634 6.54028 11.471 7.13448 11.471 7.74968C11.5218 7.74968 13.0648 7.74948 13.786 7.74968C14.1136 7.74968 14.2256 7.86228 14.2256 8.19168C14.2256 8.99588 14.2254 9.80028 14.2248 10.6045C14.2246 10.9291 14.1194 11.0327 13.7898 11.0329C13.0686 11.0333 11.5316 11.0331 11.4644 11.0331V17.5421C11.4644 17.8891 11.3552 17.9997 11.0128 17.9997C10.178 17.9997 9.34298 17.9999 8.50818 17.9997C8.20558 17.9997 8.08758 17.8821 8.08758 17.5795C8.08738 15.4597 8.08738 11.1075 8.08738 11.0331Z" fill="#3D6AD6"/>
+                                    </svg>
+                                </a>
+                            </div>
+                            <div>
+                                <a href="" class="baiviet__social__icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                                        <g clip-path="url(#clip0_1_116565)">
+                                            <path d="M20 3.79875C19.2563 4.125 18.4637 4.34125 17.6375 4.44625C18.4875 3.93875 19.1363 3.14125 19.4412 2.18C18.6488 2.6525 17.7738 2.98625 16.8412 3.1725C16.0887 2.37125 15.0162 1.875 13.8462 1.875C11.5763 1.875 9.74875 3.7175 9.74875 5.97625C9.74875 6.30125 9.77625 6.61375 9.84375 6.91125C6.435 6.745 3.41875 5.11125 1.3925 2.6225C1.03875 3.23625 0.83125 3.93875 0.83125 4.695C0.83125 6.115 1.5625 7.37375 2.6525 8.1025C1.99375 8.09 1.3475 7.89875 0.8 7.5975C0.8 7.61 0.8 7.62625 0.8 7.6425C0.8 9.635 2.22125 11.29 4.085 11.6712C3.75125 11.7625 3.3875 11.8062 3.01 11.8062C2.7475 11.8062 2.4825 11.7913 2.23375 11.7362C2.765 13.36 4.2725 14.5538 6.065 14.5925C4.67 15.6838 2.89875 16.3412 0.98125 16.3412C0.645 16.3412 0.3225 16.3263 0 16.285C1.81625 17.4563 3.96875 18.125 6.29 18.125C13.835 18.125 17.96 11.875 17.96 6.4575C17.96 6.27625 17.9538 6.10125 17.945 5.9275C18.7588 5.35 19.4425 4.62875 20 3.79875Z" fill="#03A9F4"/>
+                                        </g>
+                                        <defs>
+                                            <clipPath id="clip0_1_116565">
+                                                <rect width="20" height="20" fill="white"/>
+                                            </clipPath>
+                                        </defs>
+                                    </svg>
+                                </a>
+                            </div>
+                            <div>
+                                <a href="" class="baiviet__social__icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                                        <path d="M15.8463 2.92383H4.15374C1.85969 2.92383 0 4.78352 0 7.07757V12.9226C0 15.2166 1.85969 17.0763 4.15374 17.0763H15.8463C18.1403 17.0763 20 15.2166 20 12.9226V7.07757C20 4.78352 18.1403 2.92383 15.8463 2.92383ZM13.0371 10.2845L7.56814 12.8928C7.42241 12.9623 7.25408 12.8561 7.25408 12.6947V7.31488C7.25408 7.15115 7.42684 7.04503 7.57287 7.11905L13.0418 9.89045C13.2044 9.97284 13.2016 10.206 13.0371 10.2845Z" fill="#F61C0D"/>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+    <div class="uk-container uk-margin-large-top">
+        <h2 class="uk-text-center baiviet__title" style="--icon-title-bottom: url(images/Frame_427322377.png)">Bài viết liên quan khác</h2>
+        <div class="uk-margin-medium-top" uk-slider="finite: true; sets: true">
 
+            <div class="uk-position-relative">
+
+                <div class="baiviet__slider-container-offset uk-slider-container-offset uk-slider-container">
+                    <ul class="uk-slider-items uk-child-width-1-2@s uk-child-width-1-3@m uk-grid-small uk-grid-20-m" uk-grid>
+                        <?php for ($i=1;$i<=5;$i++): ?>
+                        <li>
+                            <div class="baiviet__card uk-card uk-padding-small uk-card-default uk-card-body">
+                                <div class="uk-flex uk-flex-column row-gap-24">
+                                    <div class="uk-flex uk-flex-column row-gap-16">
+                                        <div class="baiviet__card__coverImg uk-cover-container">
+                                            <img src="images/noimage.jpg" alt="" uk-cover="">
+                                            <canvas width="389" height="260"></canvas>
+                                        </div>
+                                        <h4 class="baiviet__card__title uk-margin-remove"><a href="">Những lưu ý khi học bảng phiên âm quốc tế IPA có thể bạn chưa biết?</a></h4>
+                                        <div class="baiviet__card__time" style="--icon-datetime: url(images/calendar-interface-symbol-tool_1.png)">26/06/2023</div>
+                                    </div>
+                                    <div style="--icon-more: url(images/arrow-right_5_1.png)">
+                                        <a class="baiviet__card__linkMore uk-button uk-button-text" href="#">Chi tiết bài viết</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <?php endfor; ?>
+                    </ul>
+                </div>
+
+                <div class="uk-hidden@s uk-light">
+                    <a uk-icon="icon: arrow-left; ratio: 1.3" class="baiviet__icon baiviet__icon--previous uk-position-center-left uk-position-small" href="#" uk-slidenav-previous1 uk-slider-item="previous"></a>
+                    <a uk-icon="icon: arrow-right; ratio: 1.3" class="baiviet__icon baiviet__icon--next uk-position-center-right uk-position-small" href="#" uk-slidenav-next1 uk-slider-item="next"></a>
+                </div>
+
+                <div class="uk-visible@s">
+                    <a uk-icon="icon: arrow-left; ratio: 1.3" class="baiviet__icon baiviet__icon--previous uk-position-center-left-out uk-position-small" href="#" uk-slidenav-previous1 uk-slider-item="previous"></a>
+                    <a uk-icon="icon: arrow-right; ratio: 1.3" class="baiviet__icon baiviet__icon--next uk-position-center-right-out uk-position-small" href="#" uk-slidenav-next1 uk-slider-item="next"></a>
+                </div>
+
+            </div>
+
+            <ul class="uk-slider-nav uk-hidden@s uk-dotnav uk-flex-center uk-margin uk-position-relative"></ul>
+
+        </div>
+    </div>
+</div>
 <?php require "template-parts/layouts/footer.php"; ?>
