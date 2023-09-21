b57879c6593e7c257b54cf3d4b7ba2b2cf941d8b
hailm <hai.lemanh.1310@gmail.com>
2023-09-21 10:12:46 +0700

    feat: block Ý nghĩa logo và câu chuyện thương hiệu
    Ý nghĩa logo và câu chuyện thương hiệu

--- about.php
+++ about.php
@@ -11,4 +11,21 @@
     </div>
 </div>
 <!--/Về Casalink-->
+
+<!--Ý nghĩa logo và câu chuyện thương hiệu-->
+<div class="uk-section">
+    <div class="uk-container">
+        <div class="uk-child-width-1-2@m uk-flex-middle" uk-grid>
+            <div>
+                <img src="images/about/Group45447-3.png" alt="">
+            </div>
+            <div>
+                <h2 class="uk-text-left baiviet__title" style="--icon-title-bottom: url(images/Frame_427322377.png); align-items: flex-start">Ý nghĩa logo và câu chuyện thương hiệu</h2>
+                <p class="home__sugia__txt">Biểu tượng logo của CASALink thể hiện mục tiêu xây dựng môi trường học tập lý tưởng giúp trẻ tự tin phát triển năng lực của bản thân, trở thành nhà vô địch của chính mình.
+                    Với hình ảnh cái khiên tượng trưng cho sự bảo vệ các e mầm non tương lai của đất nước, giúp học viên tiếp bước trong hành trang giáo dục. Chiếc vương miện ở bên trên cho thấy mỗi học viên đều chứa trong mình một tài năng tiềm ẩn và phát huy nó để trở thành nhà vô địch của chính mình. Ngọn núi biểu thị những khó khăn, thử thách và Casalink chính là người bạn đồng hành với học viên để vượt qua thử thách, chinh phục đỉnh cao mới.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/Ý nghĩa logo và câu chuyện thương hiệu-->
 <?php require "template-parts/layouts/footer.php"; ?>
