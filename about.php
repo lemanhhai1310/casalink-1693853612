74f32e89e3f0d60d562a6b0e9d0867cb59f5ea84
hailm <hai.lemanh.1310@gmail.com>
2023-09-21 10:06:10 +0700

    feat: block about__banner

--- about.php
+++ about.php
@@ -2,5 +2,13 @@
 <?php $bodyClass = '' ?>
 <?php $isLogin = false ?>
 <?php require "template-parts/layouts/header.php"; ?>
-
+<!--Về Casalink-->
+<div class="about__banner uk-flex uk-flex-middle uk-light uk-background-norepeat uk-background-center-center uk-background-image@m" data-src="images/about/Group45446-5.png" uk-img>
+    <div class="uk-section-small uk-width">
+        <div class="uk-container">
+            <h1 class="uk-text-center baiviet__title" style="--icon-title-bottom: url(images/tailieu/Frame_427322377-2.png)">Về Casalink</h1>
+        </div>
+    </div>
+</div>
+<!--/Về Casalink-->
 <?php require "template-parts/layouts/footer.php"; ?>
